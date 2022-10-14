3ae0d8e9c1cb7c84a57ea42e4bbeaa00e1d2fd01
FR_Jess <fr_jess@icloud.com>
2022-10-14 17:11:57 +0200

    [add] mail check

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// **Mail**
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+// const lista mail autorizzate
+
+const mailList = [
+  "jessica.tartaglia@gmail.com",
+  "laura.bolle@gmail.com",
+  "matteo.tasca@gmail.com",
+  "lorenzo.porta@gmail.com",
+  "alessandra.bianchi@gmail.com",
+  "anna.grande@gmail.com",
+  "alessia.tartaglia@gmail.com",
+  "andrea.giugno@gmail.com",
+  "ilaria.fedeli@gmail.com",
+  "emma.pace@gmail.com"
+];
+
+// le const di cui ho bisogno
+let present = true;
+const submitButton = document.getElementById("btn-input");
+const userMail = document.getElementById("userMail");
+const output = document.getElementById("output");
+
+// input mail utente con click
+submitButton.addEventListener("click", function(){
+
+  for(let i = 0; i < mailList.lenght; i++){
+  
+    if (userMail === mailList[i]){
+      present = true;
+      break; 
+    }
+    else{
+      present = false;
+    }
+  }
+
+  if(present){
+    output.innerHTML = 'Buona fortuna!';
+}
+  else{
+    output.innerHTML = 'Ci dispiace, non sei autorizzatə a giocare! Inrerire una mail valida.';
+}
+})
