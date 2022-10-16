4c8e26b990c110eac282d934955b89d120baaf2b
FR_Jess <fr_jess@icloud.com>
2022-10-16 19:36:21 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -16,47 +16,67 @@ const mailList = [
 ];
 
 // if mail is not present on authorized list
-
 let present = true;
 
 // button submit check mail
 const submitButton = document.getElementById("btn-input");
 
 // usermail
-const userMail = document.getElementById("userMail");
+const userMail = document.getElementById("userMail").value;
 
 // output for usermail
-const output = document.getElementById("output-mail");
+const outputMail = document.getElementById("output-mail");
 
 //FUNCTION CLICK TO CHECK MAIL AUTHORIZATION
 submitButton.addEventListener("click", function(){
 
-  // Condition for mail present
-  if (userMail !== ""){
-    for(let i = 0; i < mailList.lenght; i++){
-
-      //Conditional statement for authorization
-      if (userMail === mailList[i]){
-        present = true;
-        break; 
-      }
-      else{
-        present = false;
-      }
-    }
-    
-    // Output print on page
-    if(present){
-      output.innerHTML = 'Buona fortuna!';
-    }
-    else{
-      output.innerHTML = 'Ci dispiace, non sei autorizzatə a giocare! Inserire una mail valida.';
+  //Conditional statement for authorization
+  for(let i = 0; i < mailList.lenght; i++){
+    if (userMail === mailList[i]){
+      present = true;
+      break; 
     }
   }
-
-  // If no mail present ---- da verificare perché non funziona
+  
+  // Output print on page
+  if(present){
+    outputMail.innerHTML = 'Buona fortuna!';
+  }
   else{
-    output.innerHTML = 'Nessuna mail inserita! Inserire una mail valida.';
-  }
-    
+    outputMail.innerHTML = 'Ci dispiace, non sei autorizzatə a giocare! Inserire una mail valida.';
+  } 
 });
+
+
+// //FUNCTION CLICK TO CHECK MAIL AUTHORIZATION WITH EMPTY MAIL OPTION
+// submitButton.addEventListener("click", function(){
+
+//   // Condition for mail present
+//   if (userMail !== ""){
+//     for(let i = 0; i < mailList.lenght; i++){
+
+//       //Conditional statement for authorization
+//       if (userMail === mailList[i]){
+//         present = true;
+//         break; 
+//       }
+//       else{
+//         present = false;
+//       }
+//     }
+    
+//     // Output print on page
+//     if(present){
+//       output.innerHTML = 'Buona fortuna!';
+//     }
+//     else{
+//       output.innerHTML = 'Ci dispiace, non sei autorizzatə a giocare! Inserire una mail valida.';
+//     }
+//   }
+
+//   // If no mail present ---- da verificare perché non funziona
+//   else{
+//     output.innerHTML = 'Nessuna mail inserita! Inserire una mail valida.';
+//   }
+    
+// });
